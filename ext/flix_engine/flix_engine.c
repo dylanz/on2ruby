60600ea33ddbccc876519ab0759e64652340573f
Dylan Stamat <dstamat@air.local>
2009-01-09 12:56:59 +1300

    there be ghosts in this here git.  add muxer back to enable flv

--- ext/flix_engine/flix_engine.c
+++ ext/flix_engine/flix_engine.c
@@ -148,9 +148,11 @@ VALUE FlixEngine_get_flix_errors(VALUE self)
 VALUE FlixEngine_enable_flv(VALUE self)
 {
   FLIX2PLGNHANDLE codec;
+  FLIX2PLGNHANDLE muxer;
 
   CHECKSC( Flix2_AddCodec(&codec, flix, FE2_CODEC_VP6) )
   CHECKSC( Flix2_CodecSetParam(codec, FE2_VP6_BITRATE, 450.0) )
+  CHECKSC( Flix2_AddMuxer(&muxer, flix, FE2_MUXER_FLV) )
 }
 
 
